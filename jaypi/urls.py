4cd0bb12d34f10cd42626ede742477b755a4aeb7
Joao Pimentel <mail@joaopimentel.net>
2015-12-09 11:40:50 +0100

    explicit mezzanine.urls and custom blog.urls modeules

--- jaypi/urls.py
+++ jaypi/urls.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.http import HttpResponse
 
+from mezzanine.core.sitemaps import DisplayableSitemap
 from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 
@@ -26,9 +28,6 @@ if settings.USE_MODELTRANSLATION:
     )
 
 urlpatterns += patterns('',
-    # We don't want to presume how your homepage works, so here are a
-    # few patterns you can use to set it up.
-
     # HOMEPAGE AS STATIC TEMPLATE
     # ---------------------------
     # This pattern simply loads the index.html template. It isn't
@@ -37,62 +36,86 @@ urlpatterns += patterns('',
     # one out.
 
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-
-    # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
-    # ---------------------------------------------
-    # This pattern gives us a normal ``Page`` object, so that your
-    # homepage can be managed via the page tree in the admin. If you
-    # use this pattern, you'll need to create a page in the page tree,
-    # and specify its URL (in the Meta Data section) as "/", which
-    # is the value used below in the ``{"slug": "/"}`` part.
-    # Also note that the normal rule of adding a custom
-    # template per page with the template name using the page's slug
-    # doesn't apply here, since we can't have a template called
-    # "/.html" - so for this case, the template "pages/index.html"
-    # should be used if you want to customize the homepage's template.
-
-    # url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
-
-    # HOMEPAGE FOR A BLOG-ONLY SITE
-    # -----------------------------
-    # This pattern points the homepage to the blog post listing page,
-    # and is useful for sites that are primarily blogs. If you use this
-    # pattern, you'll also need to set BLOG_SLUG = "" in your
-    # ``settings.py`` module, and delete the blog page object from the
-    # page tree in the admin if it was installed.
-
-    # url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
-
-    # MEZZANINE'S URLS
-    # ----------------
-    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
-    # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
-    # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
-    # WILL NEVER BE MATCHED!
-
-    # If you'd like more granular control over the patterns in
-    # ``mezzanine.urls``, go right ahead and take the parts you want
-    # from it, and use them directly below instead of using
-    # ``mezzanine.urls``.
-    ("^", include("mezzanine.urls")),
-
-    # MOUNTING MEZZANINE UNDER A PREFIX
-    # ---------------------------------
-    # You can also mount all of Mezzanine's urlpatterns under a
-    # URL prefix if desired. When doing this, you need to define the
-    # ``SITE_PREFIX`` setting, which will contain the prefix. Eg:
-    # SITE_PREFIX = "my/site/prefix"
-    # For convenience, and to avoid repeating the prefix, use the
-    # commented out pattern below (commenting out the one above of course)
-    # which will make use of the ``SITE_PREFIX`` setting. Make sure to
-    # add the import ``from django.conf import settings`` to the top
-    # of this file as well.
-    # Note that for any of the various homepage patterns above, you'll
-    # need to use the ``SITE_PREFIX`` setting as well.
-
-    # ("^%s/" % settings.SITE_PREFIX, include("mezzanine.urls"))
-
 )
+
+# Include mezzanine.urls explicitly
+
+# JavaScript localization feature
+js_info_dict = {'domain': 'django'}
+urlpatterns += patterns('django.views.i18n',
+    (r'^jsi18n/(?P<packages>\S+?)/$', 'javascript_catalog', js_info_dict),
+)
+
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        urlpatterns += patterns('',
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        )
+
+# Django's sitemap app.
+if "django.contrib.sitemaps" in settings.INSTALLED_APPS:
+    sitemaps = {"sitemaps": {"all": DisplayableSitemap}}
+    urlpatterns += patterns("django.contrib.sitemaps.views",
+        ("^sitemap\.xml$", "sitemap", sitemaps)
+    )
+
+# Return a robots.txt that disallows all spiders when DEBUG is True.
+if getattr(settings, "DEBUG", False):
+    urlpatterns += patterns("",
+        ("^robots.txt$", lambda r: HttpResponse("User-agent: *\nDisallow: /",
+                                                content_type="text/plain")),
+    )
+
+# Miscellanous Mezzanine patterns.
+urlpatterns += patterns("",
+    ("^", include("mezzanine.core.urls")),
+    ("^", include("mezzanine.generic.urls")),
+)
+
+# Mezzanine's Accounts app
+_old_accounts_enabled = getattr(settings, "ACCOUNTS_ENABLED", False)
+if _old_accounts_enabled:
+    import warnings
+    warnings.warn("The setting ACCOUNTS_ENABLED is deprecated. Please "
+                  "add mezzanine.accounts to INSTALLED_APPS.")
+if _old_accounts_enabled or "mezzanine.accounts" in settings.INSTALLED_APPS:
+    # We don't define a URL prefix here such as /account/ since we want
+    # to honour the LOGIN_* settings, which Django has prefixed with
+    # /account/ by default. So those settings are used in accounts.urls
+    urlpatterns += patterns("",
+        ("^", include("mezzanine.accounts.urls")),
+    )
+
+# Mezzanine's Blog app.
+# Plugin LaterPay here
+blog_installed = "mezzanine.blog" in settings.INSTALLED_APPS
+if blog_installed:
+    BLOG_SLUG = settings.BLOG_SLUG.rstrip("/")
+    blog_patterns = patterns("",
+        ("^%s" % BLOG_SLUG, include("jaypi.custom_blog_urls")),
+    )
+    urlpatterns += blog_patterns
+
+# Mezzanine's Pages app.
+PAGES_SLUG = ""
+if "mezzanine.pages" in settings.INSTALLED_APPS:
+    # No BLOG_SLUG means catch-all patterns belong to the blog,
+    # so give pages their own prefix and inject them before the
+    # blog urlpatterns.
+    if blog_installed and not BLOG_SLUG:
+        PAGES_SLUG = getattr(settings, "PAGES_SLUG", "pages").strip("/") + "/"
+        blog_patterns_start = urlpatterns.index(blog_patterns[0])
+        urlpatterns[blog_patterns_start:len(blog_patterns)] = patterns("",
+            ("^%s" % str(PAGES_SLUG), include("mezzanine.pages.urls")),
+        )
+    else:
+        urlpatterns += patterns("",
+            ("^", include("mezzanine.pages.urls")),
+        )
 
 # Adds ``STATIC_URL`` to the context of error pages, so that error
 # pages can use JS, CSS and images.
